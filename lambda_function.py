8883b9e8e2d693c988c239e6d492ce90f1ef16f1
yktakaha4 <20282867+yktakaha4@users.noreply.github.com>
2019-01-12 23:39:28 +0900

    initial commit

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+def lambda_handler(event, context):
+
+    operations = {
+        'DELETE': lambda dynamo, x: dynamo.delete_item(**x),
+        'GET': lambda dynamo, x: dynamo.scan(**x),
+        'PUT': lambda dynamo, x: dynamo.update_item(**x),
+    }
+
+    operation = event['httpMethod']
+    if operation in operations:
+        payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
+        return respond(None, operations[operation](dynamo, payload))
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
